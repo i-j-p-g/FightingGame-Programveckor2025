20a99846f0cd5e1680ae927d62e356d4ccd001d1
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2025-01-09 12:15:36 +0100

    la till kod

--- Assets/Damage.cs
+++ Assets/Damage.cs
@@ -10,7 +10,7 @@ public class Damage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth; // Sätter startvärdet på hälsan
     }
 
     // Update is called once per frame
