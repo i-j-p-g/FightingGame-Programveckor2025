b28476e8bb1230a251509c2ba7900b529b63dc24
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2025-01-09 12:14:25 +0100

    La till olika scripts

---
+++ Assets/Damage.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+
+{
+    public int maxHealth = 3; // Maximal hälsa
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
