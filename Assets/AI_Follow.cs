de907f8fd991944f63d9a0a5d6b025adc3e4bb76
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2025-01-10 10:13:27 +0100

    update ai

--- Assets/AI_Follow.cs
+++ Assets/AI_Follow.cs
@@ -23,7 +23,7 @@ public class AI_Follow : MonoBehaviour
         direction.Normalize();
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if (distance < 13)
+        if (distance < 14)
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
             
