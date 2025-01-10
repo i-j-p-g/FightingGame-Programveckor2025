be1b6c80a37e898a745fb57525ce694313b32d09
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2025-01-10 10:06:50 +0100

    Update AI_Follow.cs

--- Assets/AI_Follow.cs
+++ Assets/AI_Follow.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AI_Follow : MonoBehaviour
@@ -19,7 +20,16 @@ public class AI_Follow : MonoBehaviour
     {
         distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+        if (distance < 13)
+        {
+            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+            
+
+        }
+
+        
     }
 }
