88baa0b323f50664e7852f8fb27db72c410e07eb
i-j-p-g <ian.jimenezpradosberg@elev.ga.lbs.se>
2025-01-15 16:26:13 +0100

    Update Enemy_Attack.cs

--- Assets/Enemy_Attack.cs
+++ Assets/Enemy_Attack.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class Enemy_Attack : MonoBehaviour
 {
+    public GameObject player;
+    public float speed;
+    public float attackRange = 2f;     //attack range
+    public LayerMask playerMask;
+    public Transform Enemycheck;       //position för raycasten
+    public LayerMask EnemyMask;
+    public float attackCooldown = 2f;  //tid mellan attacker
+
+    private Animator animator;
+    private bool canAttack = true;        // Attack timer
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +25,22 @@ public class Enemy_Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+
+        if (distance <= attackRange && canAttack)
+        {
+            Attack();
+        }
+        else
+        {
+            
+        }
+
+        void Attack()
+        {
+            canAttack = false; //sätter attack cooldown
+
+        }
+
     }
 }
